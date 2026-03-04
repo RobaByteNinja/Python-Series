1b02973213a94f3d3f1dec3607fe05a620c8394f
Roba Ahmed <robaalp07@gmail.com>
2026-03-04 14:20:35 +0300

    chores: finished two lesson of python tutorial

---
+++ Lesson 2/welcome.py
@@ -0,0 +1,11 @@
+line01 = '********************'  # header / footer
+line02 = '*                  *'  # re-usable
+line03 = '*     WELCOME!     *'
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+Python Series
