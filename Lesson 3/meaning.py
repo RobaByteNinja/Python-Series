76ada0e14548968a9f859689b978fd308788827e
Roba Ahmed <robaalp07@gmail.com>
2026-03-05 17:46:54 +0300

    chores: third tutor

---
+++ Lesson 3/meaning.py
@@ -0,0 +1,11 @@
+meaning = 6
+print('')
+
+# if else statement
+# if meaning > 10:
+#     print('Right on')
+# else:
+#     print('Not Today')
+
+# Ternary Operator
+print('Right on') if meaning > 10 else print('Not Today')
