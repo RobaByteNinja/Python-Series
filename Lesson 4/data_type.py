b3875643dc297d6a84c26dcc6995b1ffecf9caf4
Roba Ahmed <robaalp07@gmail.com>
2026-03-09 22:14:28 +0300

    finished lesson 4

--- Lesson 4/data_type.py
+++ Lesson 4/data_type.py
@@ -1,6 +1,7 @@
 # String data type
 
 # literal assignment
+import math
 first = 'Murad'
 last = 'Alamudin'
 
@@ -75,3 +76,62 @@ print("Tea".ljust(16, '.') + "$2".rjust(4))
 print("Milk".ljust(16, ".") + "$3".rjust(4))
 print("mango".ljust(16, ".") + "$2".rjust(4))
 print(' ')
+
+# String index
+print(first[1])
+print(first[-1])
+print(first[1:-1])
+print(first[1:])
+
+# Some method return boolean data
+print(first.startswith('D'))
+print(first.startswith('M'))
+print(first.endswith("D"))
+print(first.endswith('d'))
+
+# Boolean data type
+myvalue = True
+x = bool(False)
+print(type(x))
+print(isinstance(myvalue, bool))
+
+# Numeric data type
+
+# integer data type
+price = 100
+best_price = int(100)
+print(type(price))
+print(isinstance(best_price, int))
+
+# float data type
+gpa = 3.26
+y = float(3.26)
+print(type(gpa))
+print(isinstance(y, float))
+
+# complex data type
+comp_value = 3 + 2j
+print(type(comp_value))
+print(comp_value.imag)
+print(comp_value.real)
+
+# Built in functions for numbers
+print(abs(gpa))
+print(abs(gpa * -1))
+print(round(gpa))
+print(round(gpa, 1))
+
+print(math.pi)
+print(math.sqrt(81))
+print(math.ceil(gpa))
+print(math.floor(gpa))
+
+# Casting a string to a number
+zip_code = "3000"
+zip_code_number = int(zip_code)
+print(type(zip_code_number))
+
+# Error if you attempt to cast incorrect data
+location = 'Adama'
+or_location = int(location)
+print(or_location)
